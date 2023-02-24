1c473bcca5e6d4d70d7bf8b95ac0c9e3d33293d3
Emilio Kariuki <emilio113kariuki@gmail.com>
2023-02-24 13:24:45 +0300

    ADD: added the code for authentication of the user

---
+++ lib/Repositories/Auth.dart
@@ -0,0 +1,71 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:jobsy_flutter/Firebase/Authentication.dart';
+import 'package:jobsy_flutter/Model/UserModel.dart';
+import 'package:jobsy_flutter/Ui/Utilities/SharedPreferenceManager.dart';
+
+class Auth {
+  final _firebaseAuth = FirebaseAuth.instance;
+  Future<bool> register(
+      {required String name,
+      required String email,
+      required String password}) async {
+    User? user;
+
+    try {
+      user = (await _firebaseAuth.createUserWithEmailAndPassword(
+              email: email, password: password))
+          .user;
+
+      if (user != null) {
+        FirebaseAuthentication().createUser(
+          id: user.uid,
+            user: UserModel(
+          userId: user.uid,
+          name: name,
+          email: email,
+          createdAt: DateTime.now(),
+          phone: "0700000000",
+          bio:
+              "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.",
+          image: "https://firebasestorage.googleapis.com/v0/b/apt-rite-346310.appspot.com/o/profile.jpg?alt=media&token=71a31f30-11ee-49b9-913c-b3682d3f6ea7",
+          location: "Nairobi, Kenya",
+          role: "Developer",
+          amountEarned: "0",
+        ));
+        await SharedPreferencesManager().setUserId(value: user.uid);
+        await SharedPreferencesManager().setUserName(value: name);
+        await SharedPreferencesManager().setUserEmail(value: email);
+        await SharedPreferencesManager().setUserLocation(value: "home");
+      }
+      return true;
+    } catch (e) {
+      debugPrint(e.toString());
+      return false;
+    }
+  }
+
+  Future<bool> login({required String email, required String password}) async {
+    User? user;
+
+    try {
+      user = (await _firebaseAuth.signInWithEmailAndPassword(
+              email: email, password: password))
+          .user;
+      if (user != null) {
+        await SharedPreferencesManager().setUserId(value: user.uid);
+        await SharedPreferencesManager().setUserEmail(value: email);
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+      return false;
+    }
+  }
+
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+}
